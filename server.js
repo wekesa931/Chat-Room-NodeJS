a54f975aaf074f2320c94aa2c968e1adaf8da51e
wekesa931 <wekesabill@gmail.com>
2019-10-25 11:17:57 +0300

    push to heroku

--- server.js
+++ server.js
@@ -1,5 +1,19 @@
-const io = require('socket.io')(3000)
+const express = require('express')
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+
+const rooms = {}
+app.get("/", (req,res) => {
+  res.render('index', { rooms: rooms })
+})
+
+server.listen(3000)
 const users = {}
 
 
